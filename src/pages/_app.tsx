d5d5fa19097dc013916eecf01d8be7388007ef5c
Luke Bayliss <hello@lukebayliss.com>
2024-01-02 14:51:33 +1100

    add react query dev tools

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,8 @@ import { type Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ChakraProvider } from "@chakra-ui/react";
 import { type AppType } from "next/app";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
 
 import { api } from "~/utils/api";
 
@@ -14,6 +16,9 @@ const MyApp: AppType<{ session: Session | null }> = ({
   return (
     <SessionProvider session={session}>
       <ChakraProvider resetCSS theme={theme}>
+        {process.env.NODE_ENV !== "production" && (
+          <ReactQueryDevtools initialIsOpen={false} />
+        )}
         <Component {...pageProps} />
       </ChakraProvider>
     </SessionProvider>
